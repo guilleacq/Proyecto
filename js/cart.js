764f6ea8b30901ba157fc446d917e5937064fec1
guilleacq <guillermoacq@gmail.com>
2021-10-09 11:28:38 -0300

    Entrega 5
    - Añadido el carrito, opciones de envio y datos adicionales

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,90 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+let productosCarrito=[];
 
-});
+
+
+
+//Funcion que actualiza el subtotal cuando cambia la cantidad. precio (int), cantidad (int), subtotalId (id)
+function updateProductoSubtotal(precio, cantidad, subtotalId){
+    document.getElementById(subtotalId).innerHTML = cantidad*precio;
+    updateTotalPrice();
+}
+
+function showTotal()
+{
+    let htmlToAppend = "";
+    htmlToAppend += `
+    <tr>
+    <td></td>
+    <td class="align-middle"></td>
+    <td class="align-middle"></td>
+    <td class="align-middle"></td>
+    <td class="align-middle"><b>Total</b> <p id="total"> </p></td>
+    </tr>`
+
+
+    document.getElementById("carrito").innerHTML += htmlToAppend;
+}
+
+
+function showCarrito(){ //Muestra el carrito cargando elemento por elemento desde el JSON
+    let htmlToAppend = "";
+    let id = 0;
+    let subtotalPrice; 
+    
+    for(let article of productosCarrito){ //Carga elemento por elemento
+
+        subtotalPrice = parseInt(article.unitCost) * parseInt(article.count); //Hace el calculo del subtotal inicial
+    
+        htmlToAppend += `
+        <tr>
+        <td><img src="${article.src}" class = "img-fluid" style ="max-width:50px!important"></td>
+        <td class="align-middle">${article.name}</td>
+        <td class="align-middle">${article.currency} ${article.unitCost}</td>
+        <td class="align-middle"><input type="number" min ="1" onchange="updateProductoSubtotal(this.value, ${article.unitCost}, ${id})" value=${article.count}></td>
+        <td class="subt align-middle" id="${id}">${subtotalPrice}</td>
+        </tr>`
+        id++;              
+                       
+       
+    }
+    document.getElementById("carrito").innerHTML += htmlToAppend;
+
+    showTotal(); //Muestra el total despues de haber cargado los productos
+    updateTotalPrice(); //actualiza el precio total
+
+
+}
+
+
+
+function getCarrito(url){
+    
+    return fetch(url)
+    .then(respuesta=>{
+        return respuesta.json();
+    })
+    
+}
+
+function updateTotalPrice()
+{
+    let sum = 0;
+    let subtotals = document.getElementsByClassName("subt");
+
+    for (let sub of subtotals)
+    {
+        sum += parseInt(sub.innerHTML);
+    }
+
+    document.getElementById("total").innerHTML = sum;
+}
+
+document.addEventListener("DOMContentLoaded", function(e){ //Cuando se carga la pagina.. carga el JSON
+    getCarrito(CART_INFO_URL)
+    .then(respuesta=>{
+        productosCarrito = respuesta.articles; //Guarda los articulos del carrito en productosCarrito
+        showCarrito(); 
+    })
+})
+
+
